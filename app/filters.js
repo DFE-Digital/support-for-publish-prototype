5b3a05d509eab7998c64194626b160afff7bffac
Simon Whatley <simonwhatley@gmail.com>
2022-01-13 16:57:35 +0000

    Add core prototype kit

---
+++ app/filters.js
@@ -0,0 +1,50 @@
+const _ = require('lodash')
+
+module.exports = function (env) {
+  /**
+   * Instantiate object used to store the methods registered as a
+   * 'filter' (of the same name) within nunjucks. You can override
+   * gov.uk core filters by creating filter methods of the same name.
+   * @type {Object}
+   */
+  const filters = {}
+
+  /* ------------------------------------------------------------------
+  utility function to return true or false
+  example: {{ 'yes' | falsify }}
+  outputs: true
+  ------------------------------------------------------------------ */
+  filters.falsify = (input) => {
+    if (_.isNumber(input)) return input
+    else if (input == false) return false
+    if (_.isString(input)){
+      const truthyValues = ['yes','true']
+      const falsyValues = ['no', 'false']
+      if (truthyValues.includes(input.toLowerCase())) return true
+      else if (falsyValues.includes(input.toLowerCase())) return false
+    }
+    return input
+  }
+
+  /* ------------------------------------------------------------------
+  utility function to get an error for a component
+  example: {{ errors | getErrorMessage('title') }}
+  outputs: "Enter a title"
+  ------------------------------------------------------------------ */
+  filters.getErrorMessage = function (array, fieldName) {
+    if (!array || !fieldName) {
+      return null
+    }
+
+    const error = array.filter((obj) =>
+      obj.fieldName === fieldName
+    )[0]
+
+    return error
+  }
+
+  /* ------------------------------------------------------------------
+    keep the following line to return your filters to the app
+  ------------------------------------------------------------------ */
+  return filters
+}
